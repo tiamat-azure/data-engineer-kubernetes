afde2fc540b732a1e02e370ba082568b7b9b4c53
tiamat-azure <48587107+tiamat-azure@users.noreply.github.com>
2023-11-08 01:16:25 +0000

    fix: yaml postgres non alpine et 8080 pgadmin

--- install.sh
+++ install.sh
@@ -12,8 +12,11 @@ k3d cluster delete mycluster
 # Creer le cluster k3s
 k3d cluster create mycluster
 
+# Creer les ressources k8s pour le namespace 'standard'
+kubectl apply -f yaml-standard/all-standard.yaml
+
 # Se positionner par defaut sur le namespace 'standard'
-# kubectl config set-context --current --namespace=standard
+kubectl config set-context --current --namespace=standard
 
 # Consulter le contexte courant
 # kubectl config current-context
@@ -21,13 +24,10 @@ k3d cluster create mycluster
 # Consulter le detail du contexte courant et les autres contextes
 # kubectl config get-contexts $(kubectl config current-context)
 
-# Creer les ressources k8s pour le namespace 'standard'
-kubectl apply -f yaml-standard/all-standard.yaml
-
 # Consulter les logs de tous les pods fastapi
-kubectl logs -l app=fastapi --all-containers --previous -n standard > yaml-standard/logs-fastapi.txt
-kubectl logs -l app=pgadmin --all-containers --previous -n standard > yaml-standard/logs-pgadmin.txt
-kubectl logs -l app=postgres --all-containers --previous -n standard > yaml-standard/logs-postgres.txt
+kubectl logs -l app=fastapi --all-containers=true --tail=20 --prefix > yaml-standard/logs-fastapi.txt
+kubectl logs -l app=pgadmin --all-containers=true --tail=20 --prefix > yaml-standard/logs-pgadmin.txt
+kubectl logs -l app=postgres --all-containers=true --tail=20 --prefix > yaml-standard/logs-postgres.txt
 
 
 # Creer les ressources k8s pour le namespace helm
