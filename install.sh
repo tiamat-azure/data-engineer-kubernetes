81bd130d4fb4f1d95004d995f2d6a33e6bdb3512
tiamat-azure <48587107+tiamat-azure@users.noreply.github.com>
2023-11-08 00:56:59 +0000

    feat: ajouter le namespace standard

--- install.sh
+++ install.sh
@@ -6,8 +6,28 @@ figlet "Dev Container - Bienvenue"
 # Afficher la version de l'image de base
 uname -a
 
-# Creer les ressources k8s pour le namespace standard
-# kubectl apply -f all-standard.yaml -n standard --create-namespace
+# Supprimer le cluster k3d mycluster s'il existe
+k3d cluster delete mycluster
+
+# Creer le cluster k3s
+k3d cluster create mycluster
+
+# Se positionner par defaut sur le namespace 'standard'
+# kubectl config set-context --current --namespace=standard
+
+# Consulter le contexte courant
+# kubectl config current-context
+
+# Consulter le detail du contexte courant et les autres contextes
+# kubectl config get-contexts $(kubectl config current-context)
+
+# Creer les ressources k8s pour le namespace 'standard'
+kubectl apply -f yaml-standard/all-standard.yaml
+
+# Consulter les logs de tous les pods fastapi
+kubectl logs -l app=fastapi --all-containers --previous -n standard > yaml-standard/logs-fastapi.txt
+kubectl logs -l app=pgadmin --all-containers --previous -n standard > yaml-standard/logs-pgadmin.txt
+kubectl logs -l app=postgres --all-containers --previous -n standard > yaml-standard/logs-postgres.txt
 
 
 # Creer les ressources k8s pour le namespace helm
