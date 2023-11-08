b656adf534a2ce04b4ebb751a9e4b7e6e0cf2633
tiamat-azure <48587107+tiamat-azure@users.noreply.github.com>
2023-11-08 00:03:36 +0000

    feat: initialiser yaml standard

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Afficher un message de bienvenue avec figlet
+figlet "Dev Container - Bienvenue"
+
+# Afficher la version de l'image de base
+uname -a
+
+# Creer les ressources k8s pour le namespace standard
+# kubectl apply -f all-standard.yaml -n standard --create-namespace
+
+
+# Creer les ressources k8s pour le namespace helm
+
+
+# Creer les ressources k8s pour le namespace kustomize
+
